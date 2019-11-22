45ebf4b9cf8eba6be4a06f2add5d8b369ec893f5
Max <maksim-gutov@mail.ru>
2019-11-23 01:24:45 +0300

    Подключил Telegraf, тестовое соединение не прошло

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,13 @@
-console.log('Hello World');
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//console.log('Hello World');
+const dotenv = require("dotenv");
+const telegraf = require('telegraf');
+dotenv.config({ path: `${__dirname}/../.env` });
+const bot = new telegraf(process.env.TOKEN);
+bot.on('text', (ctx) => {
+    ctx.reply(ctx.message.text);
+});
+bot.startPolling();
+// console.log(process.env.TOKEN);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log('Hello World');
+//console.log('Hello World');
+import * as dotenv from 'dotenv'
+import {Telegraf, ContextMessageUpdate} from 'telegraf';
+const telegraf=require('telegraf');
+
+dotenv.config({path: `${__dirname}/../.env`});
+
+const bot: Telegraf<ContextMessageUpdate> = new telegraf(process.env.TOKEN);
+bot.on('text',
+    (ctx)=>{ctx.reply(ctx.message.text)
+});
+bot.startPolling();
+   // console.log(process.env.TOKEN);
