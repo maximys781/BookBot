ca5bbc0ccddbd26eef272ddbc6c5046367e3eef4
Max <maksim-gutov@mail.ru>
2020-04-02 21:59:17 +0300

    First go

--- dist/index.js
+++ dist/index.js
@@ -3,8 +3,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 //console.log('Hello World');
 const dotenv = require("dotenv");
 const telegraf = require('telegraf');
+const express = require('express');
+const expressApp = express();
 dotenv.config({ path: `${__dirname}/../.env` });
 const bot = new telegraf(process.env.TOKEN);
+expressApp.use(bot.webhookCallback('/secret-path'));
+bot.telegram.setWebhook('https://api.telegram.org/bot' + process.env.TOKEN + '/setWebhook?url=http://gutov.website/telegram-bot');
 bot.on('text', (ctx) => {
     ctx.reply(ctx.message.text);
 });
--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,14 @@
 import * as dotenv from 'dotenv'
 import {Telegraf, ContextMessageUpdate} from 'telegraf';
 const telegraf=require('telegraf');
+const express=require('express');
+const expressApp=express();
 
 dotenv.config({path: `${__dirname}/../.env`});
 
 const bot: Telegraf<ContextMessageUpdate> = new telegraf(process.env.TOKEN);
+expressApp.use(bot.webhookCallback('/secret-path'))
+bot.telegram.setWebhook('https://api.telegram.org/bot'+process.env.TOKEN+'/setWebhook?url=http://gutov.website/telegram-bot')
 bot.on('text',
     (ctx)=>{ctx.reply(ctx.message.text)
 });
