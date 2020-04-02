90482b4bb6b481bd1d71403b2fb3dc7b4d038d3e
Max <maksim-gutov@mail.ru>
2020-04-02 22:19:40 +0300

    Chetyre  go

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,12 @@
 //console.log('Hello World');
 import * as dotenv from 'dotenv'
-import {Telegraf, ContextMessageUpdate} from 'telegraf';
-const telegraf=require('telegraf');
-const express=require('express');
-const expressApp=express();
-
-dotenv.config({path: `${__dirname}/../.env`});
-
+import { Telegraf, ContextMessageUpdate } from 'telegraf';
+const telegraf = require('telegraf');
+dotenv.config({ path: `${__dirname}/../.env` });
 const bot: Telegraf<ContextMessageUpdate> = new telegraf(process.env.TOKEN);
-expressApp.use(bot.webhookCallback('/secret-path'))
-bot.telegram.setWebhook('https://api.telegram.org/bot'+process.env.TOKEN+'/setWebhook?url=http://gutov.website/telegram-bot')
 bot.on('text',
-    (ctx)=>{ctx.reply(ctx.message.text)
-});
+    (ctx) => {
+        ctx.reply(ctx.message.text)
+    });
 bot.startPolling();
    // console.log(process.env.TOKEN);
