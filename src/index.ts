ec6a0493180765e2f47f4671bb76af8d91d90571
Max <maksim-gutov@mail.ru>
2020-04-02 22:26:53 +0300

    Chetyre  go

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 //console.log('Hello World');
 import * as dotenv from 'dotenv'
-import { Telegraf, ContextMessageUpdate } from 'telegraf';
-const telegraf = require('telegraf');
-dotenv.config({ path: `${__dirname}/../.env` });
-const bot: Telegraf<ContextMessageUpdate> = new telegraf(process.env.TOKEN);
+import { Telegraf, ContextMessageUpdate } from 'telegraf'
+const telegraf = require('telegraf')
+dotenv.config({ path: `${__dirname}/../.env` })
+const bot: Telegraf<ContextMessageUpdate> = new telegraf(process.env.TOKEN)
 bot.on('text',
     (ctx) => {
         ctx.reply(ctx.message.text)
-    });
-bot.startPolling();
+    })
+bot.startPolling()
    // console.log(process.env.TOKEN);
